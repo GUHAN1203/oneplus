1661dcb982e90e08db161b594dd3a15307cbe278
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-09 09:09:23 +0530

    mnbuj

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.querySelector(".js-search-icon").addEventListener("click",function(){
-    
-    document.querySelector(".js-search-input").classList.add("hide1");
+alert("hi")
+document.querySelector(".submitButton").addEventListener("click",function(){
+    alert("hi");
 })
 
 function phone(name,type,id,price,description) {
@@ -12,14 +12,7 @@ function phone(name,type,id,price,description) {
         this.description=description;
         
     };
-phones= [] ;
 
-alert(document.querySelector("#mobileName").value )
-
-document.querySelector("#mobileSubmit").addEventListener("click",function(){
-    let phone1=  new phone("ubuu" , "mobile",1234,120000,"knneefn");
-        alert(phone1.name);
-})
 
 
 

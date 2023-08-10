5ecf9f25da26d97e4a01e4806ab290e22bcb1c17
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-08-10 08:37:18 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-document.querySelector(".js-search-icon").addEventListener("click",function(){
-    
-    document.querySelector(".js-search-input").classList.add("hide1");
-})
 
 function phone(name,type,id,price,description) {
         
@@ -12,19 +8,22 @@ function phone(name,type,id,price,description) {
         this.description=description;
         
     };
-phones= [] ;
 
 
-let oneplus_11= phone("ONEPLUS 11 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-let oneplus_11r = phone("ONEPLUS 11R 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-let oneplus_9 = phone("ONEPLUS 9 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-let oneplus_9r = phone("ONEPLUS 9R 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
 
+let oneplus_11= new phone("ONEPLUS 11 5G","8 GB RAM 128 GB Storage",1,"40,000","5g")
+let oneplus_11r = new phone("ONEPLUS 11R 5G","8 GB RAM 128 GB Storage",1,"40,000","5g")
+let oneplus_9 = new phone("ONEPLUS 9 5G","8 GB RAM 128 GB Storage",1,"40,000","5g")
+let oneplus_9r = new phone("ONEPLUS 9R 5G","8 GB RAM 128 GB Storage",1,"40,000","5g")
+
+let phones=[oneplus_11,oneplus_11r,oneplus_9,oneplus_9r]
 
 for (i in phones) {
-    for (j in phones[i]) {
-        console.log(phones[i][j])
+    for (j in phones[i]){
+        console.log( phones[i][j]);
+    }
         
     }
     
-}
+
+

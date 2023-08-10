9f664bdd2ebf4b41dd7fdac0142df430fbc1326a
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-10 08:52:29 +0530

    accordion footer

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-document.querySelector(".js-search-icon").addEventListener("click",function(){
+var acc = document.getElementsByClassName("accordion-footer");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
     
-    document.querySelector(".js-search-input").classList.add("hide1");
-})
+    this.classList.toggle("active");
 
-function phone(name,type,id,price,description) {
-        
-        this.name= name;
-        this.type=type;
-        this.id= id;
-        this.price=price;
-        this.description=description;
-        
-    };
-phones= [] ;
-
-
-let oneplus_11= phone("ONEPLUS 11 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-let oneplus_11r = phone("ONEPLUS 11R 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-let oneplus_9 = phone("ONEPLUS 9 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-let oneplus_9r = phone("ONEPLUS 9R 5G","8 GB RAM" + "128 GB Storage",1,"40,000","5g")
-
-
-for (i in phones) {
-    for (j in phones[i]) {
-        console.log(phones[i][j])
-        
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-    
+  });
 }

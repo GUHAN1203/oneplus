c4e2b37f96155cb5050988face41534afe76eeb5
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-10 09:37:33 +0530

    accordian footer

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var acc = document.getElementsByClassName("accordion-footer");
+// -------------------------footer-accordian--------------------------
+
+var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    
     this.classList.toggle("active");
-
-    
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -15,3 +14,5 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// ---------------------------------------------------
